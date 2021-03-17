0989f635f8b23544ab9e306959a69eb69ef6aa4c
anthony-diep <akd@hawaii.edu>
2021-03-16 23:21:15 -1000

    week9 changes

--- js/script.js
+++ js/script.js
@@ -38,3 +38,28 @@ const myobserver = new IntersectionObserver(entries => {
 document.querySelectorAll('.animate-on-scroll').forEach((section) => {
   myobserver.observe(section);
 });
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+let options_2 = {
+  rootMargin: "0px 0px 0px 0px"
+  // threshold: 0.25
+};
+
+const mysiteheader = document.querySelector(".site-header");
+
+const myobserver_2 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains('darksection')) {
+        mysiteheader.setAttribute('data-colorstate', 'dark')
+      } else {
+        mysiteheader.setAttribute('data-colorstate', 'light')
+      }
+    }
+  });
+
+}, options_2);
+document.querySelectorAll('header, section, footer').forEach((section) => {
+  myobserver_2.observe(section);
+});
